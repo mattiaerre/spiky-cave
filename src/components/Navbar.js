d4a767277247d2c2e8bc31440d18d9f200584979
mattia richetto <mattia.richetto@gmail.com>
2018-04-17 13:13:55 +0700

    add services page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import github from '../img/github-icon.svg';
+import logo from '../img/logo.svg';
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -21,6 +21,9 @@ const Navbar = () => (
         <Link className="navbar-item" to="/products">
           Products
         </Link>
+        <Link className="navbar-item" to="/services">
+          Services
+        </Link>
       </div>
       <div className="navbar-end">
         <a
@@ -36,6 +39,6 @@ const Navbar = () => (
       </div>
     </div>
   </nav>
-)
+);
 
-export default Navbar
+export default Navbar;
