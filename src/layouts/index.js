e777b3b24572781f931a5ebed0ba035700ff8c05
mattia richetto <mattia.richetto@gmail.com>
2018-04-17 12:36:05 +0700

    set title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import Navbar from '../components/Navbar';
+import './all.sass';
+import { name, version } from '../../package.json';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title={`${name} v${version}`} />
     <Navbar />
     <div>{children()}</div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
